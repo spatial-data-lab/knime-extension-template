725ae3dd4fee49eca71e7faee3540f6abf6ed759
Xiaokang Fu <1072534112@qq.com>
2022-12-13 15:58:12 -0500

    first commit for this

---
+++ {{ cookiecutter.repo_name }}/knime_extension/src/geospatial_ext.py
@@ -0,0 +1,16 @@
+# The root category of all {{cookiecutter.extension_name}} categories
+import knime_extension as knext
+
+# This defines the root {{cookiecutter.extension_name}} KNIME category that is displayed in the node repository
+category = knext.category(
+    path="/community",
+    level_id="geo", # this is the id of the category in the node repository #FIXME: 
+    name="{{cookiecutter.extension_name}}}",
+    description="{{cookiecutter.description}}",
+    # starting at the root folder of the extension_module parameter in the knime.yml file
+    icon="icons/icon/{{cookiecutter.extension_name}}.png",
+)
+
+
+# need import node files here
+import nodes.my_nodes_catergery
