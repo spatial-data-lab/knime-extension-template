382510266ffbd683ad65d273409eda0d2a4da83e
Xiaokang Fu <1072534112@qq.com>
2022-12-14 10:19:04 -0500

    fix level id

--- {{ cookiecutter.repo_name }}/knime_extension/src/geospatial_ext.py
+++ {{ cookiecutter.repo_name }}/knime_extension/src/geospatial_ext.py
@@ -4,8 +4,8 @@ import knime_extension as knext
 # This defines the root {{cookiecutter.extension_name}} KNIME category that is displayed in the node repository
 category = knext.category(
     path="/community",
-    level_id="geo", # this is the id of the category in the node repository #FIXME: 
-    name="{{cookiecutter.extension_name}}}",
+    level_id="{{cookiecutter.extension_name}}", # this is the id of the category in the node repository #FIXME: 
+    name="{{cookiecutter.extension_name}}",
     description="{{cookiecutter.description}}",
     # starting at the root folder of the extension_module parameter in the knime.yml file
     icon="icons/icon/{{cookiecutter.extension_name}}.png",
