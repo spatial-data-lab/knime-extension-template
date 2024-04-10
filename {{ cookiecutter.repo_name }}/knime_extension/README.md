b0dd676c4e571bc067e6317a6474b6ef7b87eb19
Xiaokang Fu <1072534112@qq.com>
2024-04-10 10:39:21 -0400

    uodate the template

--- {{ cookiecutter.repo_name }}/knime_extension/README.md
+++ {{ cookiecutter.repo_name }}/knime_extension/README.md
@@ -1,3 +1,3 @@
-# KNIME Geospatial Analytics Extension Source Code
+# KNIME {{cookiecutter.extension_name}} Extension Source Code
 
-This folder contains all files necessary to build the [KNIME Analytics Platform](https://www.knime.com/) extension for Geospatial Analytics. The `src/nodes` folder contains the Python files for the different node implementations.
+This folder contains all files necessary to build the [KNIME Analytics Platform](https://www.knime.com/) extension for {{cookiecutter.extension_name}}. The `src/nodes` folder contains the Python files for the different node implementations.
--- {{ cookiecutter.repo_name }}/knime_extension/src/nodes/my_nodes_catergery.py
+++ {{ cookiecutter.repo_name }}/knime_extension/src/nodes/my_nodes_catergery.py
@@ -6,12 +6,12 @@ import util.knime_utils as knut
 __category = knext.category(
     path="/community/{{cookiecutter.extension_name}}",
     level_id="my_nodes_catergery",
-    name="Open Datasets",
-    description="Nodes for providing open geospatial datasets.",
+    name="My first category",
+    description="Nodes for my first category",
     # starting at the root folder of the extension_module parameter in the knime.yml file
-    icon="icons/icon/OpendatasetCategory.png",
-    after="LocationAnalysis",
+    icon="icons/icon/CategoryIcon.png",
+    after="some other category",
 )
 
 # Root path for all node icons in this file
-__NODE_ICON_PATH = "icons/icon/OpenDataset/"
+__NODE_ICON_PATH = "icons/icon/OpenDataset/my_first_category"
